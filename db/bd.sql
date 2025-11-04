a0cd6a66e97d055dbee0c70d4dab85096070ba73
Caio <caio_ponick@estudante.sesisenai.org.br>
2025-11-04 07:46:53 -0300

    add - alterações no banco e no diagrama para poder fazer login

--- db/bd.sql
+++ db/bd.sql
@@ -5,42 +5,46 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema KANBAN
+-- Schema kanban-login
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema KANBAN
+-- Schema kanban-login
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `KANBAN` DEFAULT CHARACTER SET utf8 ;
-USE `KANBAN` ;
+CREATE SCHEMA IF NOT EXISTS `kanban-login` DEFAULT CHARACTER SET utf8 ;
+USE `kanban-login` ;
 
 -- -----------------------------------------------------
--- Table `KANBAN`.`usuario`
+-- Table `kanban-login`.`usuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `KANBAN`.`usuario` (
-  `idusuario` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `kanban-login`.`usuario` (
+  `idtarefa` INT NOT NULL AUTO_INCREMENT,
   `nomeUsuario` VARCHAR(45) NOT NULL,
+  `senhaUsuario` VARCHAR(45) NOT NULL,
   `emailUsuario` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idusuario`))
+  PRIMARY KEY (`idtarefa`),
+  UNIQUE INDEX `idtarefa_UNIQUE` (`idtarefa` ASC) VISIBLE,
+  UNIQUE INDEX `senhaUsuario_UNIQUE` (`senhaUsuario` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `KANBAN`.`tarefa`
+-- Table `kanban-login`.`tarefa`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `KANBAN`.`tarefa` (
+CREATE TABLE IF NOT EXISTS `kanban-login`.`tarefa` (
   `idtarefa` INT NOT NULL AUTO_INCREMENT,
-  `usuario_idusuario` INT NOT NULL,
+  `usuario_idtarefa` INT NOT NULL,
   `descricaoTarefa` VARCHAR(45) NOT NULL,
   `setor` VARCHAR(45) NOT NULL,
   `prioridade` ENUM('baixa', 'media', 'alta') NOT NULL,
   `data` DATE NOT NULL,
   `status` ENUM('a fazer', 'fazendo', 'feito') NOT NULL,
   PRIMARY KEY (`idtarefa`),
-  INDEX `fk_tarefa_usuario_idx` (`usuario_idusuario` ASC) VISIBLE,
+  UNIQUE INDEX `idtarefa_UNIQUE` (`idtarefa` ASC) VISIBLE,
+  INDEX `fk_tarefa_usuario_idx` (`usuario_idtarefa` ASC) VISIBLE,
   CONSTRAINT `fk_tarefa_usuario`
-    FOREIGN KEY (`usuario_idusuario`)
-    REFERENCES `KANBAN`.`usuario` (`idusuario`)
+    FOREIGN KEY (`usuario_idtarefa`)
+    REFERENCES `kanban-login`.`usuario` (`idtarefa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -49,5 +53,3 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-
--- db/db.php
+++ db/db.php
@@ -3,7 +3,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "root";
-    $dbname = "KANBAN";
+    $dbname = "kanban-login";
 
     try {
         $conn = new PDO("mysql: host=$servername;dbname=$dbname", $username, $password);
